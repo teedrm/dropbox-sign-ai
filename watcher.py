70b01aada6bbb30f7ab0d9f13428a69c604e9455
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-09-10 20:24:14 -0600

    added watcher to detect changes

---
+++ watcher.py
@@ -0,0 +1,27 @@
+import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from subprocess import Popen, PIPE
+
+APP_SCRIPT = "test-page.py"
+
+class FileChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path.endswith((".py", ".html")):
+            print("Changes detected. Reloading the server...")
+            os.system("pkill -f " + APP_SCRIPT)
+            time.sleep(1)
+            Popen(["python", APP_SCRIPT], stdout=PIPE, stderr=PIPE)
+
+if __name__ == "__main__":
+    event_handler = FileChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=".", recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
