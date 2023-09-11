c357495792e375f43f8d968b177671e7a14205af
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-09-10 21:09:22 -0600

    update

--- main-page.py
+++ main-page.py
@@ -2,18 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#future features check off
-future_features = [
-    {"name": "Live translation", "checked": False},
-    {"name": "Read agreements aloud", "checked": False},
-    {"name": "Auto-sign certain documents", "checked": False},
-    {"name": "Remove signatures", "checked": False},
-    {"name": "Manage document collections", "checked": False},
-]
-
 @app.route("/")
 def home():
-    return render_template("index.html", future_features=future_features)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port=8080)
