8386033cc569a4afb7057cffa2107587e7208f0b
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-09-22 00:21:21 -0600

    transcription works but super slow - took around  ~2mins

--- main-page.py
+++ main-page.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
+from pydub import AudioSegment
+from io import BytesIO
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    transcript = ""
+    if request.method == "POST":
+        if "file" not in request.files:
+            return redirect(request.url)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
 
-@app.route("/template")
-def templates():
-    return "template"
+        if file:
+            try:
+                audio = AudioSegment.from_mp3(file)
+                audio.export("temp.wav", format="wav")
+                audio_file = sr.AudioFile("temp.wav")
+                recognizer = sr.Recognizer()
+                with audio_file as source:
+                    audio_data = recognizer.record(source)
+                transcript = recognizer.recognize_google(audio_data, key=None)
+            except Exception as e:
+                transcript = f"Error: {str(e)}"
+
+    return render_template('index.html', transcript=transcript)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True, threaded=True,host='0.0.0.0', port=8080)
