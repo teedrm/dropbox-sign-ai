e3229075b612aa03f17c356955f365556e0c1d7d
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-09-22 19:12:44 -0600

    implement BART summarisation

--- main-page.py
+++ main-page.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request, redirect
+# Import necessary libraries
+from flask import Flask, render_template, request, redirect, jsonify
 import speech_recognition as sr
 from pydub import AudioSegment
 from io import BytesIO
+from transformers import BartTokenizer, BartForConditionalGeneration
 
+# Initialize the Flask app
 app = Flask(__name__)
 
+# Load BART summarization model and tokenizer
+model_name = "facebook/bart-large-cnn"
+tokenizer = BartTokenizer.from_pretrained(model_name)
+summarization_model = BartForConditionalGeneration.from_pretrained(model_name)
+
+# Define the index route
 @app.route("/", methods=["GET", "POST"])
 def index():
     transcript = ""
@@ -30,5 +39,22 @@ def index():
 
     return render_template('index.html', transcript=transcript)
 
+# Define a route for summarization
+@app.route("/summarize", methods=["POST"])
+def summarize():
+    try:
+        data = request.get_json()
+        transcript = data.get("transcript", "")
+
+        # Perform summarization using BART model
+        input_ids = tokenizer.encode("summarize: " + transcript, return_tensors="pt", max_length=1024, truncation=True)
+        summary_ids = summarization_model.generate(input_ids, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+        return jsonify({"summary": summary})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True,host='0.0.0.0', port=8080)
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=8080)
